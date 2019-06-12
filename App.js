06d59696dffba5b812ede0380683a0af760a9085
Athul Muralidhar <athul.m68@gmail.com>
2019-06-12 10:38:55 +0200

    added lifecycle methods

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date :new Date()}
+  }
+
+  componentDidMount(){
+    this.timerId = setInterval(
+      ()=> this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+  render(){
+    return(
+        <Text>Local time:{this.state.date.toLocaleTimeString()}</Text>
+    )
+  }
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Clock/>
     </View>
   );
 }
